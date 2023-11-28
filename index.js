3b018d2a7d4e9ac654f4e9355d0074e002095204
Towhidul123 <excaliburfsh@gmail.com>
2023-11-28 21:43:27 +0600

    user dashboard

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
 
     const menuCollection = client.db("techDB").collection("techProduct");
     const reviewsCollection = client.db("techDB").collection("reviews");
+    const userProduct = client.db("techDB").collection("userProduct");
 
     // Get all documents from collection
     app.get('/menu', async(req,res)=>{
@@ -63,6 +64,21 @@ async function run() {
       }
     });
 
+    //userproduct
+
+    app.post('/dashboard/userProduct', async (req, res) => {
+      try {
+        const newUserProduct = req.body;
+        const result = await userProduct.insertOne(newUserProduct);
+        res.send(result);
+      } catch (error) {
+        console.error('Error adding to cart:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
+    
+
+
     //reviews
   app.post('/reviews', async (req, res) => {
     try {
@@ -75,7 +91,19 @@ async function run() {
     }
   });
 
+  app.get('/reviews', async (req, res) => {
+    const roomId = req.query.roomId;
+    try {
+      const query = { roomId: roomId };
+      const result = await reviewsCollection.find(query).toArray();
+      res.json(result);
+    } catch (error) {
+      console.error('Error fetching reviews:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
