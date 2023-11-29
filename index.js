9bacc0c00dc19fc117982c3d85e884a8a439b10b
Towhidul123 <excaliburfsh@gmail.com>
2023-11-30 04:36:41 +0600

    search

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require ('cors');
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app. use(express.json());
+app.use(express.json());
 
 
 
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-   // await client.connect();
+    // await client.connect();
 
     const menuCollection = client.db("techDB").collection("techProduct");
     const reviewsCollection = client.db("techDB").collection("reviews");
@@ -38,57 +38,185 @@ async function run() {
 
     //jwt related api
 
-    app.post('/jwt' , async(req,res) =>{
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
-        expiresIn: '1h'});
-        res.send({token})
-      })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      });
+      res.send({ token })
+    })
 
 
 
     //insert email
 
-    app.post('/users', async(req,res)=>{
+    app.post('/users', async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
+      const query = { email: user.email }
       const existingUser = await UserCollection.findOne(query);
-     
+
       if (existingUser) {
         return res.status(409).json({ error: 'User with this email already exists' });
-    }
+      }
       const result = await UserCollection.insertOne(user);
       res.send(result);
     })
 
-//middlewares
-const verifyToken = (req, res, next) =>{
-  console.log('inside verify token', req.headers.authorization);
-  if(!req.headers.authorization){
-    return res.status(401).send({message: 'forbidden access'});
-  } 
-  const token = req.headers.authorization.split(' ')[1];
+    //middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'forbidden access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
 
-jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-  if(err){
-    return res.status(401).send({ message:'forbidden access'});
-  }
-  req.decoded = decoded;
-  next();
-})
-}
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
 
-    app.get('/users', verifyToken, async(req,res)=>{
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await UserCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
+
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       console.log(req.headers)
       const result = await UserCollection.find().toArray();
       res.send(result);
     })
 
+
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+      const query = { email: email };
+      const user = await UserCollection.findOne(query);
+      let admin = false;
+
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+
+      res.send({ admin });
+    })
+
+
+    app.get('/users/mod/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await UserCollection.findOne(query);
+      let moderator = false;
+    
+      if (user) {
+        moderator = user?.role === 'moderator';
+      }
+    
+      res.send({ moderator });
+    })
+
+
     // Get all documents from collection
-    app.get('/menu', async(req,res)=>{
+    app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
+    
+
+    //upvote button
+    app.post('/api/upvote/:productId',  async (req, res) => {
+      const productId = req.params.productId;
+  
+      try {
+          const query = { _id: new ObjectId(productId) };
+          const product = await menuCollection.findOne(query);
+  
+          if (!product) {
+              return res.status(404).json({ error: 'Product not found' });
+          }
+  
+          const updatedProduct = await menuCollection.findOneAndUpdate(
+              query,
+              { $inc: { upvote_count: 1 } }, // Increment upvote_count by 1
+              { returnDocument: 'after' } // Return the updated document
+          );
+  
+          res.json(updatedProduct.value); // Send the updated product back to the client
+      } catch (error) {
+          console.error('Error while upvoting product:', error);
+          res.status(500).json({ error: 'Internal Server Error' });
+      }
+  });
+
+
+  //report button
+
+  app.post('/api/report/:productId', async (req, res) => {
+    const productId = req.params.productId;
+
+    try {
+        const query = { _id: new ObjectId(productId) };
+        const product = await menuCollection.findOne(query);
+
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+
+        // Update the product with the report information
+        const updatedProduct = await menuCollection.findOneAndUpdate(
+            query,
+            { $set: { report: 'reported' } }, // Set the 'report' field to 'reported'
+            { returnDocument: 'after' } // Return the updated document
+        );
+
+        res.json(updatedProduct.value); // Send the updated product back to the client
+    } catch (error) {
+        console.error('Error while handling report:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+    
+  //search
+  app.get('/products', async (req, res) => {
+    const searchTerm = req.query.search;
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = 20;
+    const skip = (page - 1) * pageSize;
+  
+    let query = {};
+    if (searchTerm) {
+    
+      query = { tags: { $regex: new RegExp(searchTerm, 'i') } };
+    }
+  
+    try {
+      const result = await menuCollection
+        .find(query)
+        .skip(skip)
+        .limit(pageSize)
+        .toArray();
+  
+      res.json(result);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
 
 
     //api based on ID
@@ -128,72 +256,84 @@ jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
         res.status(500).json({ error: 'Internal Server Error' });
       }
     });
-    
+
     app.get('/dashboard/userProduct', async (req, res) => {
       const cursor = userProduct.find();
       const result = await cursor.toArray();
       res.send(result);
     })
 
-    app.delete('/dashboard/userProduct/:id', async(req,res)=>{
+    app.delete('/dashboard/userProduct/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await userProduct.deleteOne(query);
       res.send(result);
     })
 
 
     //reviews
-  app.post('/reviews', async (req, res) => {
-    try {
-      const review = req.body;
-      const result = await reviewsCollection.insertOne(review);
-      res.send(result);
-    } catch (error) {
-      console.error('Error adding review:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-  app.get('/reviews', async (req, res) => {
-    const roomId = req.query.roomId;
-    try {
-      const query = { roomId: roomId };
-      const result = await reviewsCollection.find(query).toArray();
-      res.json(result);
-    } catch (error) {
-      console.error('Error fetching reviews:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-  //menu related api
-  app.delete('/users/:id', async (req,res) =>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await UserCollection.deleteOne(query);
-    res.send(result);
-  })
-
-
-  app.patch('/users/admin/:id', async(req,res) =>{
-    const id = req.params.id;
-    const filter = {_id: new ObjectId(id) };
-    const updatedDoc = {
-      $set: {
-        role : 'admin'
+    app.post('/reviews', async (req, res) => {
+      try {
+        const review = req.body;
+        const result = await reviewsCollection.insertOne(review);
+        res.send(result);
+      } catch (error) {
+        console.error('Error adding review:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
       }
-    }
-    const result = await UserCollection.updateOne(filter, updatedDoc)
-    res.send(result);
-  })
-  
+    });
+
+    app.get('/reviews', async (req, res) => {
+      const roomId = req.query.roomId;
+      try {
+        const query = { roomId: roomId };
+        const result = await reviewsCollection.find(query).toArray();
+        res.json(result);
+      } catch (error) {
+        console.error('Error fetching reviews:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
+
+    //menu related api
+    app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await UserCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await UserCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+
+    app.patch('/users/mod/:id',verifyToken, verifyAdmin,  async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'moderator'
+        }
+      }
+      const result = await UserCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-   // await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -202,9 +342,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('server is running')
-  })
-  
-  app.listen(port, () => {
-    console.log(`server is running on port: ${port} `)
-  })
+  res.send('server is running')
+})
+
+app.listen(port, () => {
+  console.log(`server is running on port: ${port} `)
+})
