39f959815c01a23a97deb0f7f280d2befba259b2
Towhidul123 <excaliburfsh@gmail.com>
2023-11-29 13:22:10 +0600

    JWT

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require ('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -34,6 +35,18 @@ async function run() {
     const UserCollection = client.db("techDB").collection("users");
 
 
+
+    //jwt related api
+
+    app.post('/jwt' , async(req,res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: '1h'});
+        res.send({token})
+      })
+
+
+
     //insert email
 
     app.post('/users', async(req,res)=>{
@@ -48,7 +61,25 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/users', async(req,res)=>{
+//middlewares
+const verifyToken = (req, res, next) =>{
+  console.log('inside verify token', req.headers.authorization);
+  if(!req.headers.authorization){
+    return res.status(401).send({message: 'forbidden access'});
+  } 
+  const token = req.headers.authorization.split(' ')[1];
+
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+  if(err){
+    return res.status(401).send({ message:'forbidden access'});
+  }
+  req.decoded = decoded;
+  next();
+})
+}
+
+    app.get('/users', verifyToken, async(req,res)=>{
+      console.log(req.headers)
       const result = await UserCollection.find().toArray();
       res.send(result);
     })
